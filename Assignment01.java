678a0fa88426f2429e3238aeeb2c00058bf3789f
rajeshswain505 <62911905+rajeshswain505@users.noreply.github.com>
2022-11-07 12:17:19 +0530

    Add files via upload

---
+++ Assignment01.java
@@ -0,0 +1,90 @@
+
+public class Assignment01 {
+	public static void main(String[] args) {
+		int n=7;
+	for(int i = 0; i < n; i++) {    
+        for(int j = 0; j < n; j++) {  
+            if(i==0||j==(n-1)/2||i== (n-1)){  
+                System.out.print("*");    
+                }else {  
+                System.out.print(" ");  
+            } 
+	        
+        }  
+        System.out.println();  
+	}System.out.println("__________________________________");
+	for(int i = 0; i < n; i++) {    
+        for(int j = 0; j < n; j++) {  
+            if(j==0||j==(n-1)||j-i==0){  
+                System.out.print("*");    
+                }else {  
+                System.out.print(" ");  
+            } 
+	        
+        }  
+        System.out.println();  
+	}System.out.println("__________________________________");
+	for(int i = 0; i < n; i++) {    
+        for(int j = 0; j < n; j++) {  
+            if(i==0||i==(n-1)/2||i== (n-1)||j==0){  
+                System.out.print("*");    
+                }else {  
+                System.out.print(" ");  
+            } 
+	        
+        }  
+        System.out.println();  
+	}System.out.println("__________________________________");
+	//For print U
+	for(int i = 0; i < n; i++) {    
+        for(int j = 0; j < n; j++) {  
+            if(j==0||j==(n-1)||i== (n-1)){  
+                System.out.print("*");    
+                }else {  
+                System.out.print(" ");  
+            } 
+	        
+        }  
+        System.out.println();  
+	}System.out.println("__________________________________");
+	for(int i = 0; i < n; i++) {    
+        for(int j = 0; j < n; j++) {  
+            if(j==0||i==(n-1)/2 ||i==0||j==(n-1)){  
+                System.out.print("*");    
+                }else {  
+                System.out.print(" ");  
+            } 
+	        
+        }  
+        System.out.println();  
+	}System.out.println("__________________________________");
+	for(int i = 0; i < n; i++) {    
+        for(int j = 0; j < n; j++) {  
+            if(j==0||i==(n-1) ||i==0||j==(n-1)){  
+                System.out.print("*");    
+                }else {  
+                System.out.print(" ");  
+            } 
+	        
+        }  
+        System.out.println(); 
+	}
+        System.out.println("__________________________________");
+    	for(int i = 0; i < n; i++) {    
+            for(int j = 0; j < n; j++) {  
+                if(j==0||j==(n-1)||j-i==0){  
+                    System.out.print("*");    
+                    }else {  
+                    System.out.print(" ");  
+                } 
+    	        
+            }  
+            System.out.println();
+	}
+       
+	}
+
+	
+        
+        }
+	
